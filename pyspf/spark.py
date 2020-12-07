db6f7a4a9a74d9e902d366a88c4cc1f3eed11313
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-06 19:30:34 -0500

    renamed packages; add more tests

---
+++ pyspf/spark.py
@@ -0,0 +1,52 @@
+from itertools import product
+import pandas as pd
+from patsy.highlevel import dmatrices
+
+
+def get_profile(sdf):
+    """
+    Gets the field profiles of the specified Spark dataframe.
+
+    :param sdf: Spark dataframe.
+    :return: Dictionary.
+    """
+    dtypes = {k: v for k, v in sdf.dtypes}
+    cat_types = sdf.rdd \
+        .map(lambda r: r.asDict()) \
+        .flatMap(lambda r: [((k, r[k]), 1) for k, v in dtypes.items() if v == 'string']) \
+        .reduceByKey(lambda a, b: a + b) \
+        .map(lambda tup: (tup[0][0], {tup[0][1]: tup[1]})) \
+        .reduceByKey(lambda a, b: {**a, **b}) \
+        .map(lambda tup: (tup[0], [(k, v) for k, v in tup[1].items()])) \
+        .map(lambda tup: (tup[0], sorted(tup[1], key=lambda t: (t[1], t[0]), reverse=True))) \
+        .map(lambda tup: (tup[0], [t[0] for t in tup[1]])) \
+        .collect()
+    cat_types = {tup[0]: tup[1] for tup in cat_types}
+    con_types = {k: [1.0] for k, v in dtypes.items() if v != 'string'}
+    all_types = {**cat_types, **con_types}
+    return all_types
+
+
+def get_columns(formula, sdf, profile=None):
+    """
+    Gets the expanded columns of the specified Spark dataframe using the specified formula.
+
+    :param formula: Formula (R-like, based on patsy).
+    :param sdf: Spark dataframe.
+    :param profile: Profile. Default is `None` and profile will be determined empirically.
+    :return: Tuple of columns for y, X.
+    """
+    if profile is None:
+        profile = get_profile(sdf)
+
+    data = product(*(v for _, v in profile.items()))
+    columns = [k for k, _ in profile.items()]
+    df = pd.DataFrame(data, columns=columns)
+    y, X = dmatrices(formula, df, return_type='dataframe')
+
+    return list(y), list(X)
+
+
+def smatrices(formula, sdf, profile=None):
+    columns = get_columns(formula, sdf, profile=profile)
+    raise NotImplementedError()
---
+++ tests/test_spark.py
@@ -0,0 +1,178 @@
+import logging
+import random
+import unittest
+from itertools import product
+
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspf.spark import get_profile, get_columns
+
+
+class PySparkTest(unittest.TestCase):
+    """
+    PySpark test class.
+    """
+
+    @classmethod
+    def supress_py4j_logging(cls):
+        """
+        Supresses p4j logging.
+
+        :return: None.
+        """
+        logger = logging.getLogger('py4j')
+        logger.setLevel(logging.WARN)
+
+    @classmethod
+    def create_pyspark_session(cls):
+        """
+        Creates a PySpark session.
+
+        :return: PySpark session.
+        """
+        return (SparkSession.builder
+                .master('local[4]')
+                .appName('local-testing-pyspark')
+                .getOrCreate())
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Sets up the class.
+
+        :return: None.
+        """
+        cls.supress_py4j_logging()
+        cls.spark = cls.create_pyspark_session()
+        random.seed(37)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Tears down the class.
+
+        :return: None.
+        """
+        cls.spark.stop()
+
+    @staticmethod
+    def _get_profile():
+        """
+        Gets the profile of a dataset.
+
+        :return: Dictionary.
+        """
+        profile = {
+            'a': ['left', 'right'],
+            'b': ['high', 'mid', 'low'],
+            'x1': [20.0],
+            'x2': [3.0],
+            'y': [1.0]
+        }
+        return profile
+
+    @staticmethod
+    def _get_pdf():
+        """
+        Gets a Pandas dataframe based on made-up profile.
+
+        :return: Pandas dataframe.
+        """
+        profile = PySparkTest._get_profile()
+        data = product(*(v for _, v in profile.items()))
+        columns = [k for k, _ in profile.items()]
+        df = pd.DataFrame(data, columns=columns)
+
+        return df
+
+    def _get_sdf(self):
+        """
+        Gets a Spark dataframe based on made-up profile.
+
+        :return: Spark dataframe.
+        """
+        pdf = PySparkTest._get_pdf()
+        sdf = self.spark.createDataFrame(pdf)
+        return sdf
+
+
+class SparkTest(PySparkTest):
+    """
+    Tests Spark operations.
+    """
+
+    def test_get_profile(self):
+        """
+        Tests getting profile of a Spark dataframe.
+
+        :return: None.
+        """
+        sdf = self._get_sdf()
+        sdf.printSchema()
+        observed = get_profile(sdf)
+        expected = {'b': ['mid', 'low', 'high'], 'a': ['right', 'left'], 'x1': [1.0], 'x2': [1.0], 'y': [1.0]}
+
+        for k, lhs_vals in expected.items():
+            assert k in observed
+            rhs_vals = observed[k]
+
+            assert len(lhs_vals) == len(rhs_vals)
+            for v in lhs_vals:
+                assert v in rhs_vals
+
+    def test_get_columns_simple_formula_with_profile(self):
+        """
+        Tests get columns (simple) with profile specified.
+
+        :return: None.
+        """
+        formula = "y ~ x1 + x2 + C(a,levels=profile['a']) + C(b, levels=profile['b'])"
+        sdf = self._get_sdf()
+        profile = {'b': ['mid', 'low', 'high'], 'a': ['right', 'left'], 'x1': [1.0], 'x2': [1.0], 'y': [1.0]}
+
+        y_observed, X_observed = get_columns(formula, sdf, profile=profile)
+
+        y_expected = ['y']
+        X_expected = ['Intercept',
+                      "C(a, levels=profile['a'])[T.left]",
+                      "C(b, levels=profile['b'])[T.low]",
+                      "C(b, levels=profile['b'])[T.high]",
+                      'x1',
+                      'x2']
+
+        assert len(y_observed) == len(y_expected)
+        assert len(X_observed) == len(X_expected)
+
+        for y in y_observed:
+            assert y in y_expected
+
+        for x in X_observed:
+            assert x in X_expected
+
+    def test_get_columns_simple_formula_no_profile(self):
+        """
+        Tests get columns (simple) without a profile specified.
+
+        :return: None.
+        """
+        formula = "y ~ x1 + x2 + C(a,levels=profile['a']) + C(b, levels=profile['b'])"
+        sdf = self._get_sdf()
+
+        y_observed, X_observed = get_columns(formula, sdf)
+
+        y_expected = ['y']
+        X_expected = ['Intercept',
+                      "C(a, levels=profile['a'])[T.left]",
+                      "C(b, levels=profile['b'])[T.low]",
+                      "C(b, levels=profile['b'])[T.high]",
+                      'x1',
+                      'x2']
+
+        assert len(y_observed) == len(y_expected)
+        assert len(X_observed) == len(X_expected)
+
+        for y in y_observed:
+            assert y in y_expected
+
+        for x in X_observed:
+            assert x in X_expected
