95028b2a795ee04a60eb4828251d58fcf6d1c6fc
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-07 05:49:54 -0500

    upgrade

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='ydot',
-    version='0.0.1',
+    version='0.0.2',
     author='Jee Vang',
     author_email='vangjee@gmail.com',
     packages=find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests')),
